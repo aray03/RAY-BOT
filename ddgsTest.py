8bcffe37ec2a9171f08253a4e905525054718f7f
aray03 <aidan@andersenweb.com>
2026-07-26 15:38:17 -0600

    Added full searxng support through docker, updates settings to show this

--- ddgsTest.py
+++
@@ -1,8 +0,0 @@
-from ddgs import DDGS
-
-# Always use DDGS as a context manager to handle connection cleanup
-with DDGS() as ddgs:
-    results = ddgs.text("brian david gilbert zelda timeline site:youtube.com", max_results=5)
-    for result in results:
-        print(f"Title: {result['title']}")
-        print(f"URL: {result['href']}\n")
